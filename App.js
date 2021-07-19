8acf9fb56926685884072563d34590c6554007ba
Abdelnacerr <abdelnacerr@gmail.com>
2021-07-19 18:50:22 +1000

    custom bottom tabs

--- App.js
+++ App.js
@@ -3,14 +3,25 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from "./screens/Home.js";
+import Tabs from "./Navigation//Tabs.js";
+
+import Restaurant from "./screens/Restaurant.js";
+import OrderDelivery from "./screens/OrderDelivery.js";
 
 const Stack = createStackNavigator();
 
 export default function App() {
 	return (
 		<NavigationContainer>
-			<Stack.Navigator>
-				<Stack.Screen name='Home' component={Home} />
+			<Stack.Navigator
+				screenOptions={{
+					headerShown: false,
+				}}
+				initialRouteName={"Home"}
+			>
+				<Stack.Screen name='Tabs' component={Tabs} />
+				<Stack.Screen name='Restaurant' component={Restaurant} />
+				<Stack.Screen name='OrderDelivery' component={OrderDelivery} />
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
